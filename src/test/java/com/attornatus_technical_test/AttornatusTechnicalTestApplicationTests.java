d5ad0745c75d9f26743678e2057d6af100f7db5a
kleber.kaerk <apenaskleber123@gmail.com>
2023-02-25 13:19:01 -0300

    feature: criar projeto

---
+++ src/test/java/com/attornatus_technical_test/AttornatusTechnicalTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attornatus_technical_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttornatusTechnicalTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
