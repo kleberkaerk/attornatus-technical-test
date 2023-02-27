a6476585fa3b9814e22379c2e07126b8c24c5e32
kleber.kaerk <apenaskleber123@gmail.com>
2023-02-27 13:02:41 -0300

    feature: criar classe manipuladora de exceções

---
+++ src/main/java/com/attornatustechnicaltest/handler/ExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.attornatustechnicaltest.handler;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class ExceptionHandler extends ResponseEntityExceptionHandler {
+
+//    @Override
+//    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+//            MethodArgumentNotValidException ex,
+//            HttpHeaders headers,
+//            HttpStatusCode status,
+//            WebRequest request) {
+//
+//
+//        return super.handleMethodArgumentNotValid(ex, headers, status, request);
+//    }
+}
