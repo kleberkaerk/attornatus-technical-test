87de972e47f9337a4fe4736086c4c0239eeced0f
kleber.kaerk <apenaskleber123@gmail.com>
2023-02-27 13:16:06 -0300

    feature: criar objeto de resposta para exceção MethodArgumentNotValidException

---
+++ src/main/java/com/attornatustechnicaltest/exception_response/MethodArgumentNotValidExceptionDetails.java
@@ -0,0 +1,47 @@
+package com.attornatustechnicaltest.exception_response;
+
+public class MethodArgumentNotValidExceptionDetails {
+
+    private final String error;
+    private final String message;
+
+    private MethodArgumentNotValidExceptionDetails(String error, String message) {
+
+        this.error = error;
+        this.message = message;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public static final class MethodArgumentNotValidExceptionDetailsBuilder {
+        private String error;
+        private String message;
+
+        private MethodArgumentNotValidExceptionDetailsBuilder() {
+        }
+
+        public static MethodArgumentNotValidExceptionDetailsBuilder aMethodArgumentNotValidExceptionDetails() {
+            return new MethodArgumentNotValidExceptionDetailsBuilder();
+        }
+
+        public MethodArgumentNotValidExceptionDetailsBuilder error(String error) {
+            this.error = error;
+            return this;
+        }
+
+        public MethodArgumentNotValidExceptionDetailsBuilder message(String message) {
+            this.message = message;
+            return this;
+        }
+
+        public MethodArgumentNotValidExceptionDetails build() {
+            return new MethodArgumentNotValidExceptionDetails(error, message);
+        }
+    }
+}
