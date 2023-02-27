ac65cc282b11c3f2921c5e4a84fc9f44e99be4a5
kleber.kaerk <apenaskleber123@gmail.com>
2023-02-27 09:57:43 -0300

    feature: injetar PersonService em PersonController

--- src/main/java/com/attornatustechnicaltest/controller/PersonController.java
+++ src/main/java/com/attornatustechnicaltest/controller/PersonController.java
@@ -1,9 +1,19 @@
 package com.attornatustechnicaltest.controller;
 
+import com.attornatustechnicaltest.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("persons")
 public class PersonController {
+
+    private PersonService personService;
+
+    @Autowired
+    protected PersonController(PersonService personService) {
+
+        this.personService = personService;
+    }
 
 }
