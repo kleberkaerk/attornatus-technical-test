dcb5787721bb55a6ddbfbb9693fff6c77a4ae4bd
kleber.kaerk <apenaskleber123@gmail.com>
2023-02-27 09:55:42 -0300

    feature: injetar PersonRepository em PersonService

--- src/main/java/com/attornatustechnicaltest/service/PersonService.java
+++ src/main/java/com/attornatustechnicaltest/service/PersonService.java
@@ -1,7 +1,17 @@
 package com.attornatustechnicaltest.service;
 
+import com.attornatustechnicaltest.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PersonService {
+
+    private PersonRepository personRepository;
+
+    @Autowired
+    protected PersonService(PersonRepository personRepository) {
+
+        this.personRepository = personRepository;
+    }
 }
