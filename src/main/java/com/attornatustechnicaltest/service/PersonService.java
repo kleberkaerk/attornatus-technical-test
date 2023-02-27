e0ff3a0d33ee8667d7fee5406937011e4a365a71
kleber.kaerk <apenaskleber123@gmail.com>
2023-02-27 11:24:04 -0300

    feature: criar método para registrar nova pessoa em PersonService

--- src/main/java/com/attornatustechnicaltest/service/PersonService.java
+++ src/main/java/com/attornatustechnicaltest/service/PersonService.java
@@ -1,6 +1,10 @@
 package com.attornatustechnicaltest.service;
 
+import com.attornatustechnicaltest.domain.Person;
+import com.attornatustechnicaltest.dto.request.PersonRequestDTO;
+import com.attornatustechnicaltest.dto.response.PersonResponseDTO;
 import com.attornatustechnicaltest.repository.PersonRepository;
+import com.attornatustechnicaltest.util.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,5 +17,14 @@ public class PersonService {
     protected PersonService(PersonRepository personRepository) {
 
         this.personRepository = personRepository;
+    }
+
+    public PersonResponseDTO registerNewPerson(PersonRequestDTO personRequestDTO) {
+
+        Person personToBeSaved = Mapper.fromPersonRequestDTOToPerson(personRequestDTO);
+
+        Person savedPerson = this.personRepository.save(personToBeSaved);
+
+        return Mapper.fromPersonToPersonResponseDTO(savedPerson);
     }
 }
