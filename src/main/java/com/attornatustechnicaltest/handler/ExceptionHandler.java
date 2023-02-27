5f55f1fbfdbfa4737499861f2a6e52d9f1f56abd
kleber.kaerk <apenaskleber123@gmail.com>
2023-02-27 14:33:07 -0300

    feature: criar manipulador para exceção MethodArgumentNotValidException

--- src/main/java/com/attornatustechnicaltest/handler/ExceptionHandler.java
+++ src/main/java/com/attornatustechnicaltest/handler/ExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.attornatustechnicaltest.handler;
 
+import com.attornatustechnicaltest.exception_response.MethodArgumentNotValidExceptionDetails;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -8,17 +10,25 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.Objects;
+
 @RestControllerAdvice
 public class ExceptionHandler extends ResponseEntityExceptionHandler {
 
-//    @Override
-//    protected ResponseEntity<Object> handleMethodArgumentNotValid(
-//            MethodArgumentNotValidException ex,
-//            HttpHeaders headers,
-//            HttpStatusCode status,
-//            WebRequest request) {
-//
-//
-//        return super.handleMethodArgumentNotValid(ex, headers, status, request);
-//    }
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request) {
+
+        return new ResponseEntity<>(
+                MethodArgumentNotValidExceptionDetails.MethodArgumentNotValidExceptionDetailsBuilder.builder()
+                        .error(ex.getBody().getTitle())
+                        .message("Campo " +
+                                Objects.requireNonNull(ex.getFieldError()).getField() +
+                                " inválido, verifique o valor e tente novamente.")
+                        .build(),
+                HttpStatus.BAD_REQUEST);
+    }
 }
