3ac5833877bbd62d267e4f599df0607f2f9ce341
kleber.kaerk <apenaskleber123@gmail.com>
2023-02-27 17:59:50 -0300

    test: testar PersonRequestDTO

---
+++ src/test/java/com/attornatustechnicaltest/dto/request/PersonRequestDTOTest.java
@@ -0,0 +1,67 @@
+package com.attornatustechnicaltest.dto.request;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PersonRequestDTOTest {
+
+    private PersonRequestDTO personRequestDTO;
+    private PersonRequestDTO samePersonRequestDTO;
+    private PersonRequestDTO differentPersonRequestDTO;
+
+    @BeforeEach
+    void initializeObjects() {
+
+        this.personRequestDTO = PersonRequestDTO.PersonRequestDTOBuilder.builder()
+                .name("name1")
+                .dateOfBirth("01-01-2000")
+                .build();
+
+        this.samePersonRequestDTO = PersonRequestDTO.PersonRequestDTOBuilder.builder()
+                .name("name1")
+                .dateOfBirth("01-01-2000")
+                .build();
+
+        this.differentPersonRequestDTO = PersonRequestDTO.PersonRequestDTOBuilder.builder()
+                .name("name2")
+                .dateOfBirth("02-02-2002")
+                .build();
+    }
+
+    @Test
+    void getName() {
+
+        Assertions.assertThat(this.personRequestDTO.getName())
+                .isNotNull()
+                .isEqualTo("name1");
+    }
+
+    @Test
+    void getDateOfBirth() {
+
+        Assertions.assertThat(this.personRequestDTO.getDateOfBirth())
+                .isNotNull()
+                .isEqualTo("01-01-2000");
+    }
+
+    @Test
+    void testEquals() {
+
+        Assertions.assertThat(this.personRequestDTO)
+                .isEqualTo(this.samePersonRequestDTO);
+
+        Assertions.assertThat(this.personRequestDTO.equals(this.differentPersonRequestDTO))
+                .isFalse();
+    }
+
+    @Test
+    void testHashCode() {
+
+        Assertions.assertThat(this.personRequestDTO)
+                .hasSameHashCodeAs(this.samePersonRequestDTO);
+
+        Assertions.assertThat(this.personRequestDTO.hashCode())
+                .isNotEqualTo(this.differentPersonRequestDTO.hashCode());
+    }
+}
