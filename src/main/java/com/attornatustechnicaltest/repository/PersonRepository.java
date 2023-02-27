e72d935107241a666e95999c5355631dbd97b227
kleber.kaerk <apenaskleber123@gmail.com>
2023-02-27 09:50:06 -0300

    refactor: definir PersonRepository como um repositório jpa

--- src/main/java/com/attornatustechnicaltest/repository/PersonRepository.java
+++ src/main/java/com/attornatustechnicaltest/repository/PersonRepository.java
@@ -1,4 +1,9 @@
 package com.attornatustechnicaltest.repository;
 
-public interface PersonRepository {
+import com.attornatustechnicaltest.domain.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long> {
 }
