9b5dd76e791d1de5a73712570a43b5cd1f1f9e0a
kleber.kaerk <apenaskleber123@gmail.com>
2023-02-27 10:53:09 -0300

    feature: criar método para criar nova pessoa em PersonController

--- src/main/java/com/attornatustechnicaltest/controller/PersonController.java
+++ src/main/java/com/attornatustechnicaltest/controller/PersonController.java
@@ -1,7 +1,10 @@
 package com.attornatustechnicaltest.controller;
 
+import com.attornatustechnicaltest.dto.request.PersonRequestDTO;
 import com.attornatustechnicaltest.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,6 +17,12 @@ public class PersonController {
     protected PersonController(PersonService personService) {
 
         this.personService = personService;
+    }
+
+    @PostMapping("register")
+    public ResponseEntity<PersonRequestDTO> registerNewPerson(@RequestBody PersonRequestDTO personRequestDTO) {
+
+        return new ResponseEntity<>(personRequestDTO, HttpStatus.CREATED);
     }
 
 }
