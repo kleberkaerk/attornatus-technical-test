1339f2b0e132bc8b25f6f38f89ceca7e56094c14
kleber.kaerk <apenaskleber123@gmail.com>
2023-02-28 12:37:07 -0300

    test: testar contexto da aplicação

--- src/test/java/com/attornatustechnicaltest/AttornatusTechnicalTestApplicationTests.java
+++ src/test/java/com/attornatustechnicaltest/AttornatusTechnicalTestApplicationTests.java
@@ -1,13 +1,16 @@
 package com.attornatustechnicaltest;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AttornatusTechnicalTestApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
 
+        Assertions.assertThatCode(() -> AttornatusTechnicalTestApplication.main(new String[]{}))
+                .doesNotThrowAnyException();
+    }
 }
