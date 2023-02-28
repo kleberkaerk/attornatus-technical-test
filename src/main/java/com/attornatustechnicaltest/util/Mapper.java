cfc5d14f8e677b4122be1a3ecd9320bdf55c33fb
kleber.kaerk <apenaskleber123@gmail.com>
2023-02-28 16:07:06 -0300

    feature: criar métodos para mapeamento de endereço em Mapper

--- src/main/java/com/attornatustechnicaltest/util/Mapper.java
+++ src/main/java/com/attornatustechnicaltest/util/Mapper.java
@@ -1,7 +1,10 @@
 package com.attornatustechnicaltest.util;
 
+import com.attornatustechnicaltest.domain.Address;
 import com.attornatustechnicaltest.domain.Person;
+import com.attornatustechnicaltest.dto.request.AddressRequestDTO;
 import com.attornatustechnicaltest.dto.request.PersonRequestDTO;
+import com.attornatustechnicaltest.dto.response.AddressResponseDTO;
 import com.attornatustechnicaltest.dto.response.PersonResponseDTO;
 
 public class Mapper {
@@ -23,6 +26,32 @@ public class Mapper {
                 .id(person.getId())
                 .name(person.getName())
                 .dateOfBirth(person.getDateOfBirth())
+                .build();
+    }
+
+    public static Address fromAddressRequestDTOToAddress(AddressRequestDTO addressRequestDTO, Person person) {
+
+        return Address.AddressBuilder.builder()
+                .id(addressRequestDTO.getPersonId())
+                .cep(addressRequestDTO.getCep())
+                .number(addressRequestDTO.getNumber())
+                .publicPlace(addressRequestDTO.getPublicPlace())
+                .city(addressRequestDTO.getCity())
+                .isMain(addressRequestDTO.isMain())
+                .person(person)
+                .build();
+    }
+
+    public static AddressResponseDTO fromAddressToAddressResponseDTO(Address address) {
+
+        return AddressResponseDTO.AddressResponseDTOBuilder.builder()
+                .id(address.getId())
+                .cep(address.getCep())
+                .number(address.getNumber())
+                .publicPlace(address.getPublicPlace())
+                .city(address.getCity())
+                .isMain(address.isMain())
+                .person(address.getPerson())
                 .build();
     }
 }
