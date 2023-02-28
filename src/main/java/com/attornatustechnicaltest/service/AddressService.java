b6df64dfcbbedb4538c4ee310764ea63c38cc8dc
kleber.kaerk <apenaskleber123@gmail.com>
2023-02-28 15:11:01 -0300

    refactor: injetar AddressRepository em AddressService

--- src/main/java/com/attornatustechnicaltest/service/AddressService.java
+++ src/main/java/com/attornatustechnicaltest/service/AddressService.java
@@ -1,11 +1,17 @@
 package com.attornatustechnicaltest.service;
 
+import com.attornatustechnicaltest.repository.AddressRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AddressService {
 
+    private AddressRepository addressRepository;
+
     @Autowired
-    protected AddressService() {}
+    protected AddressService(AddressRepository addressRepository) {
+
+        this.addressRepository = addressRepository;
+    }
 }
