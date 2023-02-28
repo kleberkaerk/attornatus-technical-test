1a7dea13feb66d0c3629e6d8c9c3efcd695b0e07
kleber.kaerk <apenaskleber123@gmail.com>
2023-02-28 15:08:16 -0300

    feature: criar serviço para endereço

---
+++ src/main/java/com/attornatustechnicaltest/service/AddressService.java
@@ -0,0 +1,11 @@
+package com.attornatustechnicaltest.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AddressService {
+
+    @Autowired
+    protected AddressService() {}
+}
