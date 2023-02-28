d10222485985f63f73a7f8eaac99c7a75d6b83e8
kleber.kaerk <apenaskleber123@gmail.com>
2023-02-28 15:09:30 -0300

    feature: criar repositório para endereço

---
+++ src/main/java/com/attornatustechnicaltest/repository/AddressRepository.java
@@ -0,0 +1,9 @@
+package com.attornatustechnicaltest.repository;
+
+import com.attornatustechnicaltest.domain.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, Long> {
+}
