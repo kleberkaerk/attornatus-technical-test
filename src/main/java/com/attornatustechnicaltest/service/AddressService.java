818447621774937aba9d0919d7caa01fa5636427
kleber.kaerk <apenaskleber123@gmail.com>
2023-03-01 11:38:28 -0300

    refactor: remover imports inutilizados de AddressService

--- src/main/java/com/attornatustechnicaltest/service/AddressService.java
+++ src/main/java/com/attornatustechnicaltest/service/AddressService.java
@@ -8,9 +8,7 @@ import com.attornatustechnicaltest.exception.NonExistentPersonException;
 import com.attornatustechnicaltest.repository.AddressRepository;
 import com.attornatustechnicaltest.util.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
