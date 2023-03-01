414980969795c8bbbf06afcac1e658ae10e0873b
kleber.kaerk <apenaskleber123@gmail.com>
2023-03-01 17:48:49 -0300

    feature: criar método para atualizar endereço principal de usuário em AddressService

--- src/main/java/com/attornatustechnicaltest/service/AddressService.java
+++ src/main/java/com/attornatustechnicaltest/service/AddressService.java
@@ -4,6 +4,7 @@ import com.attornatustechnicaltest.domain.Address;
 import com.attornatustechnicaltest.domain.Person;
 import com.attornatustechnicaltest.dto.request.AddressRequestPost;
 import com.attornatustechnicaltest.dto.response.AddressResponse;
+import com.attornatustechnicaltest.exception.AddressNotFoundException;
 import com.attornatustechnicaltest.exception.NonExistentPersonException;
 import com.attornatustechnicaltest.repository.AddressRepository;
 import com.attornatustechnicaltest.util.Mapper;
@@ -51,5 +52,22 @@ public class AddressService {
         Address savedAddress = this.addressRepository.save(addressToBeSaved);
 
         return Mapper.fromAddressToAddressResponse(savedAddress);
+    }
+
+    public void updateMainAddress(Long personId, Long addressId) {
+
+        Person person = this.personService.findOptionalPersonById(personId)
+                .orElseThrow(() -> new NonExistentPersonException("Pessoa inexistente, por favor verifique a pessoa e tente novamente."));
+
+        Optional<Address> optionalAddress = this.addressRepository.findByPersonAndId(person, addressId);
+
+        if (optionalAddress.isEmpty()) {
+
+            throw new AddressNotFoundException("Endereço não encontrado, por favor verifique se o endereço existe, ou se ele pertence a esta pessoa, e tente novamente.");
+        }
+
+        this.deactivateAnotherMainAddress(this.addressRepository.findByPerson(person));
+
+        this.addressRepository.updateIsMainByPersonAndId(true, person, addressId);
     }
 }
