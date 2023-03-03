da528503f39a454a19593e944c6f340adb5f587c
kleber.kaerk <apenaskleber123@gmail.com>
2023-03-02 23:03:22 -0300

    feature: documentar métodos de PersonController

--- src/main/java/com/attornatustechnicaltest/controller/PersonController.java
+++ src/main/java/com/attornatustechnicaltest/controller/PersonController.java
@@ -3,12 +3,20 @@ package com.attornatustechnicaltest.controller;
 import com.attornatustechnicaltest.dto.request.PersonRequestPost;
 import com.attornatustechnicaltest.dto.request.PersonRequestPut;
 import com.attornatustechnicaltest.dto.response.PersonResponse;
+import com.attornatustechnicaltest.exception_response.NonExistentPersonExceptionDetails;
 import com.attornatustechnicaltest.service.PersonService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,19 +33,28 @@ public class PersonController {
         this.personService = personService;
     }
 
-    @GetMapping(value = "find-all")
-    public ResponseEntity<Page<PersonResponse>> findAllPerson(Pageable pageable) {
+    @GetMapping(value = "find-all", produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Busca todas as pessoas.", responses = @ApiResponse(responseCode = "200", description = "Se tudo ocorrer bem."))
+    public ResponseEntity<Page<PersonResponse>> findAllPerson(@ParameterObject Pageable pageable) {
 
         return new ResponseEntity<>(this.personService.findAllPerson(pageable), HttpStatus.OK);
     }
 
-    @GetMapping(value = "find-by-id/{personId}")
-    public ResponseEntity<PersonResponse> findPersonById(@PathVariable Long personId) {
+    @GetMapping(value = "find-by-id/{personId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Busca uma pessoa pelo id.", responses = {
+            @ApiResponse(responseCode = "200", description = "Se o id da pessoa existir no banco de dados."),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Se o id da pessoa não existir no banco de dados.",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = NonExistentPersonExceptionDetails.class)))
+    })
+    public ResponseEntity<PersonResponse> findPersonById(@PathVariable @Parameter(example = "1") Long personId) {
 
         return new ResponseEntity<>(this.personService.findPersonById(personId), HttpStatus.OK);
     }
 
-    @PostMapping(value = "register")
+    @PostMapping(value = "register", produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Cadastra uma nova pessoa.", responses = @ApiResponse(responseCode = "201", description = "Se tudo ocorrer bem."))
     public ResponseEntity<PersonResponse> registerPerson(@RequestBody @Valid PersonRequestPost personRequestPost) {
 
         return new ResponseEntity<>(
@@ -46,7 +63,14 @@ public class PersonController {
     }
 
     @PutMapping(value = "update")
-    public ResponseEntity<Void> updatePerson(@RequestBody PersonRequestPut personRequestPut) {
+    @Operation(summary = "Atualiza uma pessoa.", responses = {
+            @ApiResponse(responseCode = "204", description = "Se o id da pessoa existir no banco de dados."),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Se o id da pessoa não existir no banco de dados.",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = NonExistentPersonExceptionDetails.class)))
+    })
+    public ResponseEntity<Void> updatePerson(@RequestBody @Valid PersonRequestPut personRequestPut) {
 
         this.personService.updatePerson(personRequestPut);
 
